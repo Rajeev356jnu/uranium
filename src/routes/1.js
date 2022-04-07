2984509d78045e1f39b985036013185fa7b0109e
rajeev356jnu <rajeev356jnu@gmail.com>
2022-04-07 20:30:27 +0530

    classroom submission

---
+++ src/routes/1.js
@@ -0,0 +1,9 @@
+// let arr=['ram','shyam',"suresh","gopi","rajeev","raju"];
+// for(i=0;i<=arr.length;i++){
+   
+// } console.log(arr.length)
+
+let arr=['ram','shyam',"suresh","gopi","rajeev","raju"];
+    for(i=0;i<arr.length;i++){
+        console.log(arr[i])
+        }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const logger = require('./logger')
 
@@ -5,13 +7,46 @@ const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('------------------')
-    console.log(req)
+    // console.log(req)
     console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
+    // console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
 });
 
+// Q1 GET /all-candidates
+// Write a get api that returns a list of candidate names
 
+router.get('/all-candidates', function (req, res) {
+    let arr=["ram","shyam","suresh","gopi","rajeev","raju"];
+    let arr1=[],j=0
+    for(i=0;i<arr.length;i++){
+        arr1[j]=arr[i]
+        j++
+        }       
+        //  res.send("my name is rajeev")
+         res.send(arr1)
+});
+
+// QUES 2
+// GET /candidates?count=3
+// Write a get api that returns a list of candidates with size equal to the count value recieved in the query param.
+
+// COnsider that the count value is greater than 0 and less or equal to 10.
+
+// The request you will send should have a url like this - http://localhost:3000/candidates?count=3
+
+    router.get('/candidates', function (req, res) {
+    let arr=['ram','shyam',"suresh","gopi","rajeev","raju"];
+    let arr1=[],j=0
+    if((req.query.count>0) && (req.query.count<10)){
+        for(i=0;i<req.query.count;i++){
+            arr1[j]=arr[i]
+            j++
+        }}else{
+            res.send("invalid input")
+        }
+       res.send(arr1)
+    }) 
 
 
 module.exports = router;
