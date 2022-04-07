be00f2f4dce7094dfcab21cefb075627ca942658
rajeev356jnu <rajeev356jnu@gmail.com>
2022-04-07 13:09:15 +0530

    uptated my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('../logger/logger')
 const util = require('../util/helper')
 const validator=require("../validator/formatter")
-const_ = require('lodash');
+const lodash = require('lodash');
 
 const router = express.Router();
 
@@ -22,10 +22,33 @@ router.get('/test-me', function (req, res) {
 router.get('/hello', function (req, res) {
     console.log('I am inside the second route handler')
     res.send('My second ever api!')
-    console.log("months are divided into groups", _.arr)
-    console.log("get all but the first element of array", _.arr1)
-    console.log("an array of unique values", _.arr7)
-    console.log("object composed of key-value pairs", _.obj)
+//  Create an array of strings containing the names all the months of a year and split the array into 4 equally sized sub-arrays using the chunk function.,br>
+//  Print these sub-arrays
+let arr= ["january","feb","march","april","may","june","july","august","sep","oct","nov","dec"]
+     console.log(lodash.chunk(arr,3))
+
+    //  - Create an array containing the first 10 odd numbers. Using the tail function, return the last 9 elements of it and print them on console.
+      let arr1=[1,3,5,7,9,11,13,15,17,19]
+     console.log(lodash.tail(arr1))
+
+    //  Create 5 arrays of numbers containing a few duplicate values.<br>
+    //  Using the function union create a merged array with only unique values and print them
+      let arr2=[2,3,5,6,7];
+     let arr3=[2,3,9,0,8];
+     let arr4=[2,3,8,7,6];
+     let arr5=[2,3,5,1,0];
+     let arr6=[2,1,3,4,5,6]
+     console.log(lodash.union(arr2,arr3,arr4,arr5,arr6))
+
+    // Use the function fromPairs to create an object containing key value pairs.<br>
+    //  For example [“horror”,”The Shining"],[“drama”,”Titanic"],[“thriller”,”Shutter Island"],[“fantasy”,”Pans Labyrinth"]
+     let obj=[[ "horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+     console.log(lodash.fromPairs(obj))
+
+    
+
+
+
 });
 
 
@@ -42,4 +65,3 @@ router.get('/hello', function (req, res) {
 // });
 
 module.exports = router;
-// adding this comment for no reason
--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -17,24 +17,14 @@ word4=word3.toUpperCase();
     module.exports.word3=word3
 
 
-    let arr= ["january","feb","march","april","may","june","july","august","sep","oct","nov","dec"]
-     console.log(_.chunk(arr,3))
-     module.exports.arr=arr
-
-     let arr1=[1,3,5,7,9,11,13,15,17,19]
-     console.log(_.tail(arr1))
-     module.exports.arr1=arr1
-
-     let arr2=[2,3,5,6,7];
-     let arr3=[2,3,9,0,8];
-     let arr4=[2,3,8,7,6];
-     let arr5=[2,3,5,1,0];
-     let arr6=[2,1,3,4,5,6]
-    let arr7= console.log(_.union(arr2),(arr3),(arr4),(arr5),(arr6))
-     module.exports.arr7=arr7
+    
 
 
-     let obj=[[ "horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
-     console.log(_.fromPairs[[ "horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]])
-     module.exports.obj=obj
+   
+   
+
+   
+
+
+    
     
