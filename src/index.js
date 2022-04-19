8008bb765a8ec92ca9593b441ecfa8aaaaffcaa5
rajeev356jnu <rajeev356jnu@gmail.com>
2022-04-19 20:58:04 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const requestIp=require('request-ip')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req,res,next){
+    let date1=new Date()
+let time =date1.toLocaleDateString()
+let date =date1.toLocaleTimeString()
+let myIp=req.socket.remoteAddress
+let myApi=req.originalUrl;
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+console.log(date, time,  myIp,myApi)
+
+next()
+  })
+
+
+mongoose.connect("mongodb+srv://functionup-uranium1:QzN5Crh5t5EcxZ6T@cluster0.149kg.mongodb.net/Rajeev356mdintro?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
